0716a622ff97e6a0627e0a26a78349d26ee43567
ashrefmekni <chrefmekni2@gmail.com>
2024-04-09 16:37:56 +0200

    fix requirement typo

--- setup_external_libs.sh
+++ setup_external_libs.sh
@@ -1,3 +1,5 @@
+chmod 755 setup_external_libs.sh
+chmod 755 requirements.txt
 sudo apt update
 #sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
 #wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
@@ -9,4 +11,4 @@ sudo apt update
 #python3.9 --version
 sudo apt install python3-pip
 sudo apt install graphviz
-pip install -r requirements. txt
+pip install -r requirements.txt
